a713c32fd0b2745bd0d09ce0957bb45d43975f5d
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2017-02-01 17:09:05 +0100

    Opbeat is fixed.

--- App/app.js
+++ App/app.js
@@ -3,15 +3,13 @@ var logger = require('./logger');
 var app = express();
 
 // Catching errors Opbeat.com
-var opbeat = require('opbeat')({
+var opbeat = require('opbeat').start({
     organizationId: process.env.OPBEAT_ORGANIZATION_ID,
     appId: process.env.OPBEAT_APP_ID,
     secretToken: process.env.OPBEAT_SECRET_TOKEN
 });
 
-
 app.use(logger);
-app.use(express.static('./Web'));
 
 var jobs = require('./routes/jobs');
 app.use('/jobs', jobs);
